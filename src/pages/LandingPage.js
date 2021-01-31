d199234665dca2b254b4f54d9116ad0df0232b13
adamnain <adamdzulqarnain@gmail.com>
2021-01-31 21:02:47 +0700

    add footer

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,6 +6,7 @@ import MostPicked from "parts/MostPicked";
 import landingPage from "json/landingPage.json";
 import Categories from "parts/Categories";
 import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
 
 export default class LandingPage extends Component {
   constructor(props) {
@@ -23,6 +24,7 @@ export default class LandingPage extends Component {
         />
         <Categories data={landingPage.categories} />
         <Testimony data={landingPage.testimonial}> </Testimony>
+        <Footer />
       </>
     );
   }
